0c9f31258e1c2886b44329f32efd30d2e35c35d7
nguyentran1306 <155006442+nguyentran1306@users.noreply.github.com>
2025-05-03 13:04:59 +0700

    Fix namespace for Connection class in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Connection; // Sửa đúng namespace
+use Doctrine\DBAL\Types\Type;
 
 class AppServiceProvider extends ServiceProvider
 {
