3685e6ddc257cb712c977e0e99037f9818a720e1
nguyentran1306 <155006442+nguyentran1306@users.noreply.github.com>
2025-05-03 13:12:26 +0700

    Fix namespace for DB facade in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
 use Doctrine\DBAL\Types\Type;
 
 class AppServiceProvider extends ServiceProvider
